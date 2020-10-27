18c9e336bb9b4625c959b2db790d64886b195cf6
Jason Maxfield <thejasonmaxfield@gmail.com>
2020-10-27 16:19:16 -0700

    Finish LCC

--- __colleges/LCC.py
+++ __colleges/LCC.py
@@ -54,7 +54,15 @@ def apply(driver, student):
     ).click()
 
     WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.LINK_TEXT, "Street Address and Phone"))
+        EC.element_to_be_clickable((By.LINK_TEXT, "Name"))
+    ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "lname_id"))
+    )
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "id____UID1"))
     ).click()
 
     WebDriverWait(driver, 10).until(
@@ -66,6 +74,10 @@ def apply(driver, student):
     ).send_keys(student.cityAddress)
 
     Select(driver.find_element_by_id('stat_id')).select_by_value(student.stateAddress)
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "zip_id"))
+    ).send_keys(student.postalCode)
 
     WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.ID, "area_id"))
@@ -84,5 +96,147 @@ def apply(driver, student):
     ).click()
 
     WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "id____UID1"))
+    ).click()
+
+    while True:
+        try:
+            WebDriverWait(driver, 2).until(
+                EC.element_to_be_clickable((By.XPATH, "//*[contains(text(),'(Checklist item 3 of 6)')]"))
+            )
+            break
+        except TimeoutException:
+            try:
+                WebDriverWait(driver, 2).until(
+                    EC.element_to_be_clickable((By.XPATH, "//*[contains(text(),'Errors occurred')]"))
+                )
+                print("Bad address, trying a new one")
+                Student.get_new_address(student)
+                temp = driver.find_element_by_id("addr1_id")
+                temp.clear()
+                temp.send_keys(student.streetAddress)
+                temp = driver.find_element_by_id("city_id")
+                temp.clear()
+                temp.send_keys(student.cityAddress)
+                Select(driver.find_element_by_id('stat_id')).select_by_value(student.stateAddress)
+                temp = driver.find_element_by_id("zip_id")
+                temp.clear()
+                temp.send_keys(student.postalCode)
+                driver.find_element_by_id("id____UID1").click()
+            except TimeoutException:
+                continue
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[contains(text(),'(Checklist item 3 of 6)')]"))
+    )
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "id____UID1"))
+    ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "citz_id"))
+    )
+
+    Select(driver.find_element_by_id('citz_id')).select_by_value('01')
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "email_id"))
+    ).send_keys(student.email)
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "dup_email_id"))
+    ).send_keys(student.email)
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "ssn_id"))
+    ).send_keys(student.ssn)
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "M_gender_id"))
+    ).click()
+
+    Select(driver.find_element_by_id('month_id')).select_by_index(int(student.birthdayMonth))
+
+    Select(driver.find_element_by_id('day_id')).select_by_index(int(student.birthdayDay))
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "year_id"))
+    ).send_keys(student.birthdayYear)
+
+    if student.stateAddress == 'MI':
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "Y_resd_id"))
+        ).click()
+    else:
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "N_resd_id"))
+        ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "ud1_no_id"))
+    ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "ethn_cat_nothispanic_id"))
+    ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "race_id5"))
+    ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "id____UID1"))
+    ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "school_id"))
+    ).send_keys('230000')
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "hs_name_id"))
+    ).send_keys('Eastern High School')
+
+    Select(driver.find_element_by_id('gmonth_id')).select_by_index(int(student.eduMonth))
+
+    Select(driver.find_element_by_id('gday_id')).select_by_index(int(student.eduDay))
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "gyear_id"))
+    ).send_keys(student.eduYear)
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "id____UID2"))
+    ).click()
+
+    Select(driver.find_element_by_id('program_id')).select_by_index(random.randint(1, 150))
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "ud1_id"))
+    ).send_keys(student.firstName[0] + student.lastName[0])
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "id____UID1"))
+    ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[contains(text(),'Application is Complete')]"))
+    )
+
+    WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.ID, "id____UID0"))
     ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "I agree to the terms"))
+    ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[contains(text(),'" + student.username + "')]"))
+    )
+
+    Student.save_student(student)
+
+    print('Complete')
+
+
