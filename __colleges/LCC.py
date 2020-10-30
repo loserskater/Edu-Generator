abd952a611436868d4143b28337b9998020448f9
Jason Maxfield <thejasonmaxfield@gmail.com>
2020-10-29 21:51:44 -0700

    bugfix

--- __colleges/LCC.py
+++ __colleges/LCC.py
@@ -3,7 +3,6 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import time
 import random
 import Student
 
